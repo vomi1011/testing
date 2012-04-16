d8b7b3ddb9d3433f0256abf6921eb3278e5fb71a
Elena Remizova <chuzzla@192.168.1.7>
2012-04-16 22:19:39 +0200

    package-info

---
+++ swe/src/main/java/de/swe/artikelverwaltung/service/package-info.java
@@ -0,0 +1,13 @@
+@XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED,
+           namespace = ARTIKELVERWALTUNG_NS,
+           xmlns = @XmlNs(prefix = "", namespaceURI = ARTIKELVERWALTUNG_NS))
+@XmlAccessorType(FIELD)
+
+package de.swe.artikelverwaltung.service;
+import static de.swe.util.Constants.ARTIKELVERWALTUNG_NS;
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
