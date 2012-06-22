97f3a72eab9cc79d07dda1b004bad2c601eceac5
Priscila Stroemsdoerfer <stpr1011@hs-karlsruhe.de>
2012-06-22 16:53:57 +0200

    package-info.java für Bestellverwaltung

---
+++ swe/src/main/java/de/swe/bestellverwaltung/rest/package-info.java
@@ -0,0 +1,13 @@
+@XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED,
+           namespace = BESTELLVERWALTUNG_NS,
+           xmlns = @XmlNs(prefix = "", namespaceURI = BESTELLVERWALTUNG_NS))
+@XmlAccessorType(FIELD)
+
+package de.swe.bestellverwaltung.rest;
+import static de.swe.util.Constants.BESTELLVERWALTUNG_NS;
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
